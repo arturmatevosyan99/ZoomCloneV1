55b15ecd7985d7ce8a62ff1e2b8d91c5dbd081b6
Artur Matevosyan <artur.matevosyan99@gmail.com>
2021-09-29 03:10:22 +0400

    make it better

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -19,7 +19,7 @@ export default function Main() {
 
   return (
     <div ref={rootNode}>
-      <h1>Available Rooms</h1>
+      <h1>Available Rooms lllll</h1>
 
       <ul>
         {rooms.map(roomID => (
