1ad41d32a3aa8fd2fccd546bda3fa06b47226ed9
Artur Matevosyan <artur.matevosyan99@gmail.com>
2021-09-29 02:58:27 +0400

    fix

--- src/socket/index.js
+++ src/socket/index.js
@@ -7,6 +7,6 @@ const options = {
   transports : ["websocket"]
 }
 
-const socket = io('http://localhost:3001', options);
+const socket = io('http://localhost:' + process.env.PORT, options);
 
 export default socket;
